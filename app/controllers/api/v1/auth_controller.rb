e32e582227d40c465c8f35a28a07b88d3d83cc14
cfernando415 <ca.fernando415@gmail.com>
2019-01-31 14:45:04 -0500

    working on auth

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::AuthController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+    def create
+        @member = Member.find_by(email: auth_params[:email])
+        if @member && @member.authenticate(auth_params[:password])
+            @token = issue_token(@member.email)
+            render json: { member: UserSerializer.new(@member), jwt: @token }, status: :accepted
+        else
+            render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def auth_params
+        params.require(:auth).permit(:email, :password)
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,40 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+    def issue_token(payload)
+        JWT.encode(payload, Date.today.strftime('%Y-%m-%d'))
+      end 
+    
+      def auth_header
+        request.headers['Authorization']
+      end 
+    
+      def decoded_token
+        if auth_header
+          token = auth_header
+          begin 
+            JWT.decode(token, Date.today.strftime('%Y-%m-%d'), true, algorithm: 'HS256')
+          rescue JWT::DecodeError
+            nil 
+          end 
+        end 
+      end 
+    
+      def current_user 
+        if decoded_token
+          email = decoded_token[0]['email']
+          @user = User.find_by(email: email)
+        else
+          nil
+        end
+      end 
+    
+      def logged_in?
+        !!current_user
+      end
+    
+      def authorized
+        if !logged_in?
+          render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+        end
+      end
 end
