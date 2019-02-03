56aad142893a6e512364d31075015225bfacbeb9
cfernando415 <ca.fernando415@gmail.com>
2019-02-02 21:13:58 -0500

    member registration: completed

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::MembersController < ApplicationController
+    skip_before_action :authorized, only: [:index, :create]
     def index
         @members = Member.all
         render json: @members
     end
+    def create
+        @member = Member.create(members_params)
+        if @member.valid?
+            @token = issue_token({ email: @member.email})
+            render json: { member: MemberSerializer.new(@member), jwt: @token }, status: :created
+        else
+            render json: { error: 'Registration failed' }, status: :not_acceptable
+        end
+    end
+
+    private
+    def members_params
+        params.require(:user_info).permit(:first_name, :last_name, :email, :password)
+    end
+    
 end
