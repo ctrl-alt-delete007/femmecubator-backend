a2c43e60bc516cd975319ad894e39a0bcb45dd3c
cfernando415 <ca.fernando415@gmail.com>
2019-02-04 00:12:33 -0500

    coupon create: done

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::CouponsController < ApplicationController
         @coupons = Coupon.all
         render json: @coupons
     end
+    def create
+        @coupon = Coupon.create(coupons_params)
+        render json: CouponSerializer.new(@coupon)
+    end
+
+    private
+    def coupons_params
+        params.require(:coupon_info).permit(:coupon_code, :sponsor, :description, :expiration, :member_id)
+    end
 end
--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,6 +1,6 @@
 class CouponSerializer < ActiveModel::Serializer
   attributes :id, :coupon_code, :sponsor, :description, :expiration
   belongs_to :member
-  has_many: likes
+  has_many :likes
   has_many :members, through: :likes
 end
