ad2880df67c22234dd9cb3e4b6e88bed81f93575
cfernando415 <ca.fernando415@gmail.com>
2019-02-04 00:13:13 -0500

    coupon create: done

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CouponsController < ApplicationController
     def index
         @coupons = Coupon.all
-        render json: @coupons
+        render json: CouponSerializer.new(@coupons)
     end
     def create
         @coupon = Coupon.create(coupons_params)
